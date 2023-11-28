ec76fa1a96e9357b370f536aa7df063450d996ba
yarcello <yarcello@gmail.com>
2023-11-28 21:49:10 +0000

    added api and updater html response

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import WeatherApp from "./WeatherApp";
 export default function App() {
   return (
     <div className="App">
-      <WeatherApp />
+      <WeatherApp city="London" />
     </div>
   );
 }
