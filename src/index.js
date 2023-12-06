efbe63a0f0531eef2cff269fa864478c9c22602a
yarcello <yarcello@gmail.com>
2023-12-06 18:09:43 +0000

    uploaded background image

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import background from "./clouds.jpg";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <img src={background} className="img img-fluid" />
     <App />
   </React.StrictMode>
 );
